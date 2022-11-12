351a2d2753b4d0c40eb9b79b94505e629ba73e16
yarqui <108986486+yarqui@users.noreply.github.com>
2022-11-12 11:14:33 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+const categories = document.querySelector("#categories");
+console.log("Number of categories: ", categories.children.length);
+
+const listEl = document.querySelector("li.item");
+console.log(listEl);
+
+console.console.log();
+// listEl.forEach;
+
+// текст заголовку елемента(тегу < h2 >)
+// кількість елементів в категорії(усіх < li >, вкладених в нього).
+// --const categoryTitle = document.querySelectorAll("h2");
+// --const categoryListItem = document.querySelector("li.item");
+// --console.log(categoryListItem);
+// // console.log(categoryList.children);
+
+// --categoryListItem.forEach((element) => {
+//   --console.log("Category: ", element.innerHTML);
+//   console.log(categoryList);
+// });
+//
+// categoryTitle.forEach((title) => {
+//   console.log(title.textContent);
+//   console.log(title.textContent);
+// });
