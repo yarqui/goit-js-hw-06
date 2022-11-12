721344f77b446ab3606d56050fce9121ba4be7f5
yarqui <108986486+yarqui@users.noreply.github.com>
2022-11-12 14:38:10 +0200

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,9 @@
-const categories = document.querySelector("#categories");
-console.log("Number of categories: ", categories.children.length);
+const categoriesRef = document.querySelector("#categories");
+console.log("Number of categories: ", categoriesRef.children.length);
 
-const listEl = document.querySelector("li.item");
-console.log(listEl);
+const categoryElementRef = document.querySelectorAll(".item");
 
-console.console.log();
-// listEl.forEach;
-
-// текст заголовку елемента(тегу < h2 >)
-// кількість елементів в категорії(усіх < li >, вкладених в нього).
-// --const categoryTitle = document.querySelectorAll("h2");
-// --const categoryListItem = document.querySelector("li.item");
-// --console.log(categoryListItem);
-// // console.log(categoryList.children);
-
-// --categoryListItem.forEach((element) => {
-//   --console.log("Category: ", element.innerHTML);
-//   console.log(categoryList);
-// });
-//
-// categoryTitle.forEach((title) => {
-//   console.log(title.textContent);
-//   console.log(title.textContent);
-// });
+categoryElementRef.forEach((element) => {
+  console.log("Category: ", element.firstElementChild.textContent);
+  console.log("Elements: ", element.lastElementChild.children.length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsRef = document.querySelector("#ingredients");
+
+const ingredientsArray = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("item");
+
+  return ingredientItem;
+});
+ingredientsRef.append(...ingredientsArray);
