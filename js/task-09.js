2cf0afdb1524d7477b4130b270fec01f929501c9
yarqui <y.pelykh@gmail.com>
2022-11-13 16:15:33 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,9 +1,13 @@
 const btnRef = document.querySelector("button.change-color");
 const textSpanRef = document.querySelector("span.color");
+const bodyRef = document.querySelector("body");
 
 const getRandomHexColor = () =>
   `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-const onChangeColor = () => (textSpanRef.textContent = getRandomHexColor());
+const onChangeColor = () => {
+  textSpanRef.textContent = getRandomHexColor();
+  bodyRef.style.backgroundColor = textSpanRef.textContent;
+};
 
 btnRef.addEventListener("click", onChangeColor);
